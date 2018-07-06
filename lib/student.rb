4393faf17ea3cf5e34f2e21431500efca2af8987
Cristina Lugo <cmlugoce@syr.edu>
2018-07-06 16:48:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,7 @@ def save
   new_student
 end
 
-def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
-end
+
 
 def update
    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
@@ -65,5 +61,7 @@ def update
     grade = row[2]
     new_student = self.new(name, grade, id)
   end
+  
+  
 
 end 
